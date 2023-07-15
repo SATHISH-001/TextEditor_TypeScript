7ba12a188f5d79809ef8dfa318165f27c03daced
SATHISH-001 <sk00567800@gmail.com>
2023-07-15 18:24:05 +0530

    Rich-text-editor edited

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import MyEditor from "./MyEditor";
+import './App.css'
+
+const App = () => {
+    console.log("heloo");
+    
+    return (
+        <div>
+            <MyEditor />
+        </div>
+    )
+}
+
+export default App
---
+++ src/MyEditor.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { convertToRaw, EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "draft-js/dist/Draft.css";
+import draftToHtml from "draftjs-to-html";
+// import { BlockPicker } from 'react-color';
+
+
+const MyEditor: React.FC = () => {
+    const [editorState, setEditorState] = useState<EditorState>(
+        EditorState.createEmpty()
+    );
+
+    const onEditorStateChange = (newEditorState: EditorState) => {
+        console.log(newEditorState);
+        setEditorState(newEditorState);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">Rich Text Editor Example</header>
+            <div style={{ height: '100%' }}>
+                <Editor
+                    editorState={editorState}
+                    onEditorStateChange={onEditorStateChange}
+                    wrapperClassName="wrapper-class"
+                    editorClassName="editor-class"
+                    toolbarClassName="toolbar-class"
+                    editorStyle={{ border: "1px solid black", height: "30em" }} />
+            </div>
+            <div className="code-view">
+                <p>HTML View </p>
+                <textarea
+                    className="text-area"
+                    disabled
+                    value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default MyEditor;
